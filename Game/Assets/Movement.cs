7fc4746115dbf21bebaa3d796edbfc1c3dc43b49
frogboffin <frogboffin@gmail.com>
2015-02-01 20:20:37 +0000

    Added super basic movement
    contains one script and 2 placeholder sprites

--- Game/Assets/Movement.cs
+++ Game/Assets/Movement.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+	public float speed = 1;
+	Vector2 dir = new Vector2(0,0);
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			dir.y = speed;
+		} 
+		else if (Input.GetKey (KeyCode.S)) 
+		{
+			dir.y = -speed;
+		}
+		else if (Input.GetKey(KeyCode.A))
+		{
+			dir.x = -speed;
+		}
+		else if (Input.GetKey (KeyCode.D))
+		{
+			dir.x = speed;
+		}
+		else
+		{
+			dir = Vector2.zero;
+		}
+
+		transform.Translate (dir); 
+
+
 	}
 }
