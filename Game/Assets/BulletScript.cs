cb61ea5ae21a3ca5cd4f978adb8391395b47b8ad
Terror-Byte <liamswain4@gmail.com>
2015-02-02 17:27:51 +0000

    Added range-based bullet destruction.
    Bullets are now destroyed after a certain range, and not after a certain
    number of updates.

--- Game/Assets/BulletScript.cs
+++ Game/Assets/BulletScript.cs
@@ -3,20 +3,50 @@ using System.Collections;
 
 
 public class BulletScript : MonoBehaviour {
-	int countdownToDestruction = 0;
+	// int countdownToDestruction = 0;
+
+	Vector2 startPos;
+	Vector2 currentPos;
+	public float range = 10.0f;
 
 	// Use this for initialization
 	void Start () {
-
+		startPos = gameObject.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		/* Old time-based destruction
 		countdownToDestruction++;
 		if (countdownToDestruction == 50) {
-			GameObject bullet = GameObject.Find("BulletPrefab");
 			Destroy (gameObject);
-
 		}
+		*/
+
+		/* New distance-based destruction */
+		currentPos = new Vector2 (gameObject.transform.position.x, gameObject.transform.position.y);
+
+		float currentDist = Vector2.Distance (currentPos, startPos);
+		Debug.Log ("Start Pos: " + startPos.ToString () + " Current Pos: " + currentPos.ToString ());
+
+		if (currentDist > range) {
+			Debug.Log (currentDist);
+			Destroy(gameObject);
+		}
+		/*
+		if (Norm () >= range) {
+			Destroy(gameObject);
+		}
+		*/
 	}
+
+	float Norm() {
+		// float xComponent = Mathf.Pow (startPos.x - currentPos.x, 2);
+		// float yComponent = Mathf.Pow (startPos.y - currentPos.y, 2);
+
+		float xComponent = Mathf.Pow (currentPos.x - startPos.x, 2);
+		float yComponent = Mathf.Pow (currentPos.y - startPos.y, 2);
+		return Mathf.Sqrt (xComponent + yComponent);
+	}
+
 }
--- Game/Assets/Movement.cs
+++ Game/Assets/Movement.cs
@@ -8,11 +8,13 @@ public class Movement : MonoBehaviour {
 	Vector2 dir = new Vector2(0,0);
 
 	public float weaponSpd = 2.5f;
+	public int weaponRefresh = 25;
+	int refreshCounter;
 
 	// Use this for initialization
 	void Start () 
 	{
-		
+		refreshCounter = 0;
 	}
 	
 	// Update is called once per frame
@@ -46,32 +48,32 @@ public class Movement : MonoBehaviour {
 		// If left button pressed, generate a new bullet and fire.
 		if (Input.GetMouseButton(0))
 		{
-			//GameObject worldScript = GameObject.Find ("WorldControllerObj");
-			//WorldController worldController = worldScript.GetComponent<WorldController>();
-			//GameObject bulletObj = GameObject.Find ("BulletPrefab");
-			//BulletScript bulletScript = bulletObj.GetComponent<BulletScript>();	
-
-			GameObject player = GameObject.Find ("PlayerPrefab");
-			float xPos = player.transform.position.x;
-			float yPos = player.transform.position.y;
-
-			GameObject newBullet = (GameObject)Instantiate(bulletPrefab, new Vector3(xPos, yPos, 1), Quaternion.identity);
-			newBullet.AddComponent("Rigidbody2D");
-			newBullet.rigidbody2D.gravityScale = 0f;
-
-			// http://answers.unity3d.com/questions/736511/shoot-towards-mouse-in-unity2d.html
-
-			//Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			//Vector3 playerPos = Camera.main.WorldToScreenPoint(player.transform.position);
-			//Vector2 forceDirection = new Vector2(mousePos.x, mousePos.y) - new Vector2(playerPos.x, playerPos.y);
-			//forceDirection.Normalize();
-
-			Vector3 playerPos = Camera.main.WorldToScreenPoint(player.transform.position);
-			Vector3 mousePos = Input.mousePosition;
-			Vector3 forceDirection = mousePos - playerPos;
-			forceDirection.Normalize ();
-
-			newBullet.rigidbody2D.velocity = forceDirection * weaponSpd;
+			if (refreshCounter == 0)
+			{
+				GameObject player = GameObject.Find ("PlayerPrefab");
+				float xPos = player.transform.position.x;
+				float yPos = player.transform.position.y;
+				
+				GameObject newBullet = (GameObject)Instantiate(bulletPrefab, new Vector3(xPos, yPos, 1), Quaternion.identity);
+				newBullet.AddComponent("Rigidbody2D");
+				newBullet.rigidbody2D.gravityScale = 0f;
+				
+				Vector3 playerPos = Camera.main.WorldToScreenPoint(player.transform.position);
+				Vector3 mousePos = Input.mousePosition;
+				Vector3 forceDirection = mousePos - playerPos;
+				forceDirection.Normalize ();
+				
+				newBullet.rigidbody2D.velocity = forceDirection * weaponSpd;
+				refreshCounter++;
+			}
+			else if (refreshCounter == weaponRefresh)
+			{
+				refreshCounter = 0;
+			}
+			else
+			{
+				refreshCounter++;
+			}
 		}
 	}
 }
