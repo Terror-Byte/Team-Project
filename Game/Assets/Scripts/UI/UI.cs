7c8ea23d41f3eb15274411eefdb1b25348439285
frogboffin <frogboffin@gmail.com>
2015-02-13 01:00:46 +0000

    Added title scene

---
+++ Game/Assets/Scripts/UI/UI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+
+    int width = Screen.width;
+    int height = Screen.height;
+    public GUISkin customSkin;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.skin = customSkin;
+
+        if(GUI.Button(new Rect(width/2 - 250, height/2 - 30, 500, 60), "Start game"))
+        {
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+    }
+}
