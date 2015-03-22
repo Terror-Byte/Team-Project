c7c71fac21353aeba4f71da1d6ea427dc1cb3419
Terror-Byte <Liamswain4@gmail.com>
2015-03-22 22:18:57 +0000

    UI Stuff
    Buttons on selector now increase players level.

--- Game/Assets/Scripts/UI/SelectorUI.cs
+++ Game/Assets/Scripts/UI/SelectorUI.cs
@@ -4,23 +4,52 @@ using System.Collections;
 
 public class SelectorUI : MonoBehaviour {
 
-    Text healthText;
-    Text strengthText;
-    Text speedText;
+    public Button healthButton;
+    public Button strengthButton;
+    public Button speedButton;
+
+    public Text healthText;
+    public Text strengthText;
+    public Text speedText;
 
     GameController game;
 
-	// Use this for initialization
 	void Start () {
-        healthText = GameObject.Find("Health Up").GetComponent<Text>();
-        strengthText = GameObject.Find("Strength Up").GetComponent<Text>();
-        speedText = GameObject.Find("Speed Up").GetComponent<Text>();
+
+        //healthButton = GameObject.Find("Health Up").GetComponent<Button>();
+        //strengthButton = GameObject.Find("Strength Up").GetComponent<Button>();
+        //speedButton = GameObject.Find("Speed Up").GetComponent<Button>();
+
+        healthButton.onClick.AddListener(() => HealthUp());
+        strengthButton.onClick.AddListener(() => StrengthUp());
+        speedButton.onClick.AddListener(() => SpeedUp());
+
+        healthText = healthButton.GetComponent<Text>();
+        strengthText = strengthButton.GetComponent<Text>();
+        speedText = speedButton.GetComponent<Text>();
+
+        healthText.text = "Health Up: 1 Gold";
+        strengthText.text = "Strength Up: 1 Gold";
+        speedText.text = "Speed Up: 1 Gold";
 
         game = GameObject.Find("GameController").GetComponent<GameController>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void HealthUp()
+    {
+        game.hpLvl += 1;
+        game.gold -= 1;
+    }
+
+    void StrengthUp()
+    {
+        game.strength += 1;
+        game.gold -= 1;
+    }
+
+    void SpeedUp()
+    {
+        game.speed += 1;
+        game.gold -= 1;
+    }
 }
